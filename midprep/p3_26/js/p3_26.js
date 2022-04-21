80ad4eaafe76479c278dcb52cd5ce4b00a806b7e
WeiYiHuangfelix <a0908882001@gmail.com>
2022-04-21 08:17:59 +0800

    w09 midprep starter

---
+++ midprep/p3_26/js/p3_26.js
@@ -0,0 +1,62 @@
+const links = document.querySelectorAll('#heading a');
+
+console.log('links', links);
+
+const tku60Text = document.querySelector('#main ul');
+console.log('tku60Links', tku60Text);
+
+
+const heading = document.querySelector('#heading');
+const iframeContainer = document.querySelector('.iframe-container');
+const showImages = document.querySelector('.show-images');
+const tkuImages = document.querySelector('tku-images');
+console.log('tkuImages', tkuImages);
+
+const changeVideo = (index) => {
+    switch (index) {
+        case 1:
+            iframeContainer.innerHTML = `
+            <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/RechrtUxfQc"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+          `;
+        break;
+        case 2:
+        break;
+    }
+}
+
+const changeColor = () => {
+    heading.getElementsByClassName.backgoundColor = '#9AADBF'
+}
+
+const addTku60Text = () => {
+    tku60Text.innerHTML = `
+    <li><a onclick="changeVideo(1)">淡江大戲 1</a></li>
+    <li><a onclick="changeVideo(2)">淡江大戲 2</a></li>
+    <li><a onclick="changeVideo(3)">淡江大戲 3</a></li>
+    <li><a onclick="changeVideo(4)">淡江大戲 4</a></li>
+    <li><a onclick="changeVideo(5)">淡江大戲 5</a></li>
+    `;
+    console.log('tku60Text innerHTML', tku60Text);
+}
+
+const showAllImages = () => {
+    showImages.innerHTML = `
+    <img src="./images/TKU1.png"  width ="16%" alt="">
+    <img src="./images/TKU2.png"  width ="16%" alt="">
+    <img src="./images/TKU3.png"  width ="16%" alt="">
+    <img src="./images/TKU4.png"  width ="16%" alt="">
+    <img src="./images/TKU5.png"  width ="16%" alt="">
+    `;
+}
+
+
+links[0].addEventListener('click', changeColor);
+links[1].addEventListener('click', showAllImages);
+links[2].addEventListener('click', addTku60Text);
